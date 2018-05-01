d30c0ec06f79930a28b1feb5b36e96dd2049a58d
Eden Dupont <eg7eg7@gmail.com>
2018-05-01 14:48:51 +0300

    Fixed unittest

--- proj.py
+++ proj.py
@@ -19,12 +19,14 @@ def half(matrix, k=1):
     return matrix
 
 
-def half_test():
-    assert (half([], 0) == [])
-    assert (half([], 1) == [])
-    assert (half([[1, 2, 3, 4, 5], [6, 7, 8, 9, "spam"], [11, 12, 13, 14, 15], [16, "stam", 18, 19, 20]]) == [[1], [6, 7], [11, 12, 13], [16, "stam", 18, 19]])
-    assert (half([[1, 2, 3, 4, 5], [6, 7, 8, 9, "spam"], [11, 12, 13, 14, 15], [16, "stam", 18, 19, 20]], 0) == [[1, 2, 3, 4, 5], [7,  8,  9, "spam"], [13, 14, 15], [19, 20]])
-    assert (half([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [6, 7, 8, 9, "spam"], [11, 12, 13, 14, 15], [16, "stam", 18, 19, 20]], 0) == [[1, 2, 3, 4, 5], [2, 3, 4, 5], [8, 9, "spam"], [14, 15], [20]])
+class testHW(unittest.TestCase):
+
+    def test_half(self):
+        self.assertEqual(half([], 0), [])
+        self.assertEqual(half([], 1), [])
+        self.assertEqual(half([[1, 2, 3, 4, 5], [6, 7, 8, 9, "spam"], [11, 12, 13, 14, 15], [16, "stam", 18, 19, 20]]), [[1], [6, 7], [11, 12, 13], [16, "stam", 18, 19]])
+        self.assertEqual(half([[1, 2, 3, 4, 5], [6, 7, 8, 9, "spam"], [11, 12, 13, 14, 15], [16, "stam", 18, 19, 20]], 0), [[1, 2, 3, 4, 5], [7,  8,  9, "spam"], [13, 14, 15], [19, 20]])
+        self.assertEqual(half([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [6, 7, 8, 9, "spam"], [11, 12, 13, 14, 15], [16, "stam", 18, 19, 20]], 0), [[1, 2, 3, 4, 5], [2, 3, 4, 5], [8, 9, "spam"], [14, 15], [20]])
 
 
 # helper function - free to use where needed to debug
@@ -38,6 +40,6 @@ def print_matrix(matrix):
 
 
 if __name__ == "__main__":
-    half_test()
+    unittest.main()
 
 
