a11a422ae89a3664d90115a42e628624aa0f2707
Eden Dupont <eg7eg7@gmail.com>
2018-05-04 23:08:06 +0300

    Added Q2

--- proj.py
+++ proj.py
@@ -2,22 +2,32 @@ import unittest
 
 
 def half(matrix, k=1):
-    return [
-        row_list[row_index:len(row_list)]
-        if k==0
-        else row_list[0:row_index+1] if k==1
-        else [] for row_index, row_list in enumerate(matrix)
-    ]
+    return [row_list[row_index:len(row_list)] if k is 0 else row_list[0:row_index + 1] if k is 1 else [] for row_index, row_list in enumerate(matrix)]
 
 
-class testHW(unittest.TestCase):
+def decrypt(string, key=3):
+    new_string = ''
+    for ch in string:
+        new_string += chr(ord(ch) - key)
+    return new_string
+
+
+class TestHW(unittest.TestCase):
 
     def test_half(self):
         self.assertEqual(half([], 0), [])
         self.assertEqual(half([], 1), [])
-        self.assertEqual(half([[1, 2, 3, 4, 5], [6, 7, 8, 9, "spam"], [11, 12, 13, 14, 15], [16, "stam", 18, 19, 20]]), [[1], [6, 7], [11, 12, 13], [16, "stam", 18, 19]])
-        self.assertEqual(half([[1, 2, 3, 4, 5], [6, 7, 8, 9, "spam"], [11, 12, 13, 14, 15], [16, "stam", 18, 19, 20]], 0), [[1, 2, 3, 4, 5], [7,  8,  9, "spam"], [13, 14, 15], [19, 20]])
-        self.assertEqual(half([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [6, 7, 8, 9, "spam"], [11, 12, 13, 14, 15], [16, "stam", 18, 19, 20]], 0), [[1, 2, 3, 4, 5], [2, 3, 4, 5], [8, 9, "spam"], [14, 15], [20]])
+        self.assertEqual(half([[1, 2, 3, 4, 5], [6, 7, 8, 9, "spam"], [11, 12, 13, 14, 15], [16, "stam", 18, 19, 20]]),
+                         [[1], [6, 7], [11, 12, 13], [16, "stam", 18, 19]])
+        self.assertEqual(
+            half([[1, 2, 3, 4, 5], [6, 7, 8, 9, "spam"], [11, 12, 13, 14, 15], [16, "stam", 18, 19, 20]], 0),
+            [[1, 2, 3, 4, 5], [7, 8, 9, "spam"], [13, 14, 15], [19, 20]])
+        self.assertEqual(half(
+            [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [6, 7, 8, 9, "spam"], [11, 12, 13, 14, 15], [16, "stam", 18, 19, 20]],
+            0), [[1, 2, 3, 4, 5], [2, 3, 4, 5], [8, 9, "spam"], [14, 15], [20]])
+
+    def test_decrypt(self):
+        self.assertEqual(decrypt("vrorqjdqgwkdqnviruwkhilvk"), "solongandthanksforthefish")
 
 
 # helper function - free to use where needed to debug
@@ -32,8 +42,3 @@ def print_matrix(matrix):
 
 if __name__ == "__main__":
     unittest.main()
-    print_matrix(half([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [6, 7, 8, 9, "spam"],
-                       [11, 12, 13, 14, 15], [16, "stam", 18, 19, 20]]))
-
-
-
