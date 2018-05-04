82960145b0da86da52b4cae038f820cd80f4f44d
Eden Dupont <eg7eg7@gmail.com>
2018-05-04 23:38:17 +0300

    semantics

--- proj.py
+++ proj.py
@@ -5,13 +5,15 @@ def half(matrix, k=1):
     return [row_list[row_index:len(row_list)] if k is 0 else row_list[0:row_index + 1] if k is 1 else [] for row_index, row_list in enumerate(matrix)]
 
 
+# function decrypts only parts of the string that includes small letters of the english alphabet
 def decrypt(string, key=3):
     new_string = ''
     size_alphabet = 26
     for ch in string:
+        new_ch = ch
         if ord('a') <= ord(ch) <= ord('z'):
             new_ch = chr(ord('a') + (((ord(ch) - (key % size_alphabet)) - ord('a')) % size_alphabet))
-            new_string += new_ch
+        new_string += new_ch
     return new_string
 
 
@@ -31,21 +33,12 @@ class TestHW(unittest.TestCase):
 
     def test_decrypt(self):
         self.assertEqual(decrypt("vrorqjdqgwkdqnviruwkhilvk"), "solongandthanksforthefish")
+        self.assertEqual(decrypt("vsdp "), "spam ")
         self.assertEqual(decrypt("a"), "x")
         self.assertEqual(decrypt("a", 1), "z")
         self.assertEqual(decrypt("a", 26), "a")
         self.assertEqual(decrypt("a", 52), "a")
-
-
-
-# helper function - free to use where needed to debug
-def print_matrix(matrix):
-    print('*****Printing matrix*****')
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print(matrix[i][j], end=' ')
-        print(' ')
-    print('*****Printing matrix*****')
+        self.assertEqual(decrypt("a", 0), "a")
 
 
 if __name__ == "__main__":
