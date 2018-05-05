ef18210adb11026862522089b0df00b2076cd79e
DanRonRick <danrol92@gmail.com>
2018-05-05 18:56:03 +0300

    Q4 + Q4_test added

--- proj.py
+++ proj.py
@@ -22,7 +22,6 @@ def decrypt(string, key=3):
         new_string += new_ch
     return new_string
 
-
 def merge(iterable1, iterable2):
     res = []
     for x in iterable1:
@@ -32,6 +31,35 @@ def merge(iterable1, iterable2):
     res = sorted(res)
     for x in res:
         yield x
+
+def rank(file_name,how_to_rank='total'):
+    f=open(file_name)
+    (countries, gold, silver, bronze, results_by_rank_type, index_to_yield, res_to_yield) = ([], [], [],[], [],  0, " ")
+    (gold_weight, silver_weight, bronze_weight) = (3, 2, 1)
+
+    for index, line in enumerate(f.readlines()):
+        line_lst = line.split()
+        (countries.append(line_lst[0]), gold.append(line_lst[1]), silver.append(line_lst[2]), bronze.append(line_lst[3]))
+
+    results_by_rank_type = [
+        (int(gold[index]) + int(silver[index]) + int(bronze[index])) if how_to_rank == 'total'
+        else (int(gold[index])*gold_weight + int(silver[index])*silver_weight + int(bronze[index])*bronze_weight) if how_to_rank == 'weighted'
+        else (int(gold[index])) if how_to_rank == 'gold'
+        else -1
+        for index in range(len(countries))
+    ]
+
+    while len(results_by_rank_type)>0:
+        max_res = -1
+        for index, results in enumerate(results_by_rank_type):
+            if results_by_rank_type[index] > max_res:
+                max_res = results_by_rank_type[index]
+                index_to_yield = index
+        res_to_yield = "\"{0}\":{1}".format(countries[index_to_yield], max_res)
+        del(results_by_rank_type[index_to_yield])
+        del(countries[index_to_yield])
+        yield res_to_yield
+    f.close()
 
 class TestHW(unittest.TestCase):
 
@@ -71,6 +99,28 @@ class TestHW(unittest.TestCase):
         for i in merge(g, [2, 3, 7, 10, 11]):
             res.append(i)
         self.assertEqual(res,[0, 2, 3, 4, 7, 8, 10, 11, 12, 16, 20])
+
+    def test_rank(self):
+        file_name = 'winners_test.txt'
+        result = []
+        for nation in rank(file_name, 'total'):
+            result.append(nation)
+        self.assertEqual(result,["\"USA\":2521", "\"Great-Britain\":847", "\"Israel\":9"])
+
+        result = []
+        for nation in rank(file_name, 'weighted'):
+            result.append(nation)
+        self.assertEqual(result, ["\"USA\":5359", "\"Great-Britain\":1668", "\"Israel\":12"])
+
+        result = []
+        for nation in rank(file_name, 'gold'):
+            result.append(nation)
+        self.assertEqual(result, ["\"USA\":1022", "\"Great-Britain\":263", "\"Israel\":1"])
+
+        result = []
+        for nation in rank(file_name, 'svsd'):
+            result.append(nation)
+        self.assertEqual(result, ["\"Israel\":-1", "\"USA\":-1", "\"Great-Britain\":-1"])
 
 
 if __name__ == "__main__":
