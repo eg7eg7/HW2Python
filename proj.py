a285f9b319b3ae50f1972cc0cf99c57929ea41fa
Eden Dupont <eg7eg7@gmail.com>
2018-05-18 04:41:55 +0300

    fixed merge and default q

--- proj.py
+++ proj.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from itertools import *
-
 import logging
 
 logger = logging.getLogger(__name__)
@@ -23,19 +22,13 @@ file_path = 'winners_test.txt'
 
 '''Logs will be written in this file, if previous version exists it will be erased'''
 
-
-
 ''' function returns upper half of matrix if k=1. 
 If k=0 function returns lower half of matrix. If k!=0 and k!=1 empty list will be returned'''
 
+
 def half(matrix, k=1):
     logger.debug(f"Half: matrix = {str(matrix)}, k = {k} ")
-    result = [
-            row_list[row_index:len(row_list)] if k is 0
-            else row_list[0:row_index + 1] if k is 1
-            else []
-            for row_index, row_list in enumerate(matrix)
-    ]
+    result = [row_list[row_index:len(row_list)] if k is 0 else row_list[0:row_index + 1] if k is 1 else [] for row_index, row_list in enumerate(matrix)]
     logger.debug(f"Half result: {str(result)}")
     return result
 
@@ -62,8 +55,9 @@ def merge(iterable1, iterable2):
     for x in iterable2:
         res.append(x)
     res = sorted(list(set(res)))
+    logger.debug(f"merged iterables to list:{res}")
     for x in res:
-        logger.degug(f"Merge yield: {x}")
+        logger.debug(f"Merge yield: {x}")
         yield x
     logger.debug(f"Merge: yield finished")
     return
@@ -138,8 +132,15 @@ def str_to_matrix(matrix_as_string):
         for values in items.split():
             sublist.append(values)
         matrix.append(sublist)
-    logger.debug((f"Str_to_matrix result: str(matrix) "))
+    logger.debug(f"Str_to_matrix result: str(matrix) ")
     return matrix
+
+
+def str_to_list(string):
+    li = []
+    for x in string.split():
+        li.append(int(x))
+    return li
 
 
 class UserInterface:
@@ -161,10 +162,8 @@ class UserInterface:
     half_param2 = 0
     decrypt_param1 = "vrorqjdqgwkdqnviruwkhilvk"
     decrypt_param2 = 3
-    merge_param1 = divisable_by(4, 21)
-    merge_param1_name = "divisable by"
-    merge_param2 = [3, 4, 7, 8, 10, 11, 12, 16, 20]
-    merge_param2_name = "list"
+    merge_param1 = list(islice(divisable_by(14, 200), None))
+    merge_param2 = list(islice(divisable_by(30, 200), None))
     rank_param1 = 'winners_test.txt'
     rank_param2 = "total"
 
@@ -216,7 +215,7 @@ class UserInterface:
 
         self.text_area1.grid(row=3, column=0)
         self.text_area2.grid(row=5, column=0)
-        self.refresh_frame()
+
         # for first text area values insert
 
         self.function_output = Frame(self.root)
@@ -229,9 +228,12 @@ class UserInterface:
         self.info_label = Label(self.info_frame, text=self.text_info, justify=LEFT)
         self.info_label.grid(row=0, column=0)
 
+        self.refresh_frame()
         self.root.mainloop()
 
-    def refresh_frame(self, choice=q_list[0]):
+    def refresh_frame(self, choice=None):
+        if choice is None:
+            choice = self.q_list[self.current_option]
         logger.debug(f"Gui refresh_frame: choice = {choice}")
         self.current_option = self.q_list.index(choice)
         self.q_info_label.config(text=self.q_info_list[self.current_option])
@@ -258,26 +260,23 @@ class UserInterface:
         logger.debug(f"text_area2 data = {self.text_area2.get('1.0', END)}")
         param1 = self.text_area1.get('1.0', END)
         param2 = self.text_area2.get('1.0', END)
-
-        if self.current_option == 0:
-            param1 = str_to_matrix(param1)
-
-        if  self.current_option is 0 or self.current_option is 1:
-            param2 = int(param2)
-
-        if self.current_option == 3:
-            param1 = param1.splitlines()[0]
-            param2 = param2.splitlines()[0]
-
+        logger.info(f"Executing option{self.current_option}")
         func = self.list_of_funcs[self.current_option]
 
         if self.current_option == 0:
-            function_output = matrix_to_str(func(param1, param2), param2)
-
+            param1 = str_to_matrix(param1)
+            function_output = matrix_to_str(func(param1, int(param2)), int(param2))
         elif self.current_option == 1:
-            function_output = str(func(param1, param2))
+            function_output = str(func(param1, int(param2)))
 
-        elif self.current_option == 2 or self.current_option == 3:
+        elif self.current_option == 2:
+            param1 = str_to_list(param1)
+            param2 = str_to_list(param2)
+            function_output = str(list(islice(func(param1, param2), 0, None)))
+
+        elif self.current_option == 3:
+            param1 = param1.splitlines()[0]
+            param2 = param2.splitlines()[0]
             function_output = str(list(islice(func(param1, param2), 0, None)))
 
         string = "Result:\n"+function_output
@@ -287,5 +286,4 @@ class UserInterface:
 
 if __name__ == "__main__":
     UserInterface()
-    #TODO add exceptions and logger.error
-    #TODO solve question 3 gui problem
+    # TODO add exceptions and logger.error
